69dd558ba98a2ebe0cde67121e671d947d25c06a
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-11-07 03:48:28 +0100

    Methoden registriere und verifiziere der InstrumentenVerwaltung hinzugefügt - ValueObject VerifizierungsCode hinzugefügt

--- src/main/java/org/example/applicationservice/InstrumentenVerwaltung.java
+++ src/main/java/org/example/applicationservice/InstrumentenVerwaltung.java
@@ -1,33 +1,31 @@
 package org.example.applicationservice;
 
 import io.jexxa.addend.applicationcore.ApplicationService;
-import org.example.domain.Instrument;
-import org.example.domain.InstrumentenRepository;
-import org.example.domain.SerienNummer;
-import org.example.domain.InstrumentenDaten;
-
+import org.example.domain.*;
 import java.util.List;
 
 @ApplicationService
 public class InstrumentenVerwaltung {
     private final InstrumentenRepository instrumentenRepository;
 
-    public void add(SerienNummer seriennummer, InstrumentenDaten instrumentenDaten){
-        instrumentenRepository.add(new Instrument(instrumentenDaten, seriennummer));
+    public void add(EMailAdresse eMailAdresse, InstrumentenDaten instrumentenDaten){
+        instrumentenRepository.add(new Instrument(instrumentenDaten, eMailAdresse));
     }
     @SuppressWarnings("unused")
-    public void delete(SerienNummer serienNummer){
-        instrumentenRepository.remove(serienNummer);
+    public void delete(EMailAdresse eMailAdresse){
+        instrumentenRepository.remove(eMailAdresse);
     }
 
-    public List<SerienNummer> get(){
+    public List<EMailAdresse> get(){
         return instrumentenRepository.getAll()
                 .stream()
-                .map(Instrument::getSeriennummer)
+                .map(Instrument::getEMailAdresse)
                 .toList();
     }
 
-    public InstrumentenVerwaltung (InstrumentenRepository instrumentenRepository){
-        this.instrumentenRepository = instrumentenRepository;
+    public InstrumentenVerwaltung(InstrumentenRepository instrumentenRepository){this.instrumentenRepository = instrumentenRepository;}
+    public void registriere(EMailAdresse eMailAdresse, InstrumentenDaten instrumentenDaten){
+    }
+    public void verifiziere(EMailAdresse eMailAdresse, VerifizierungsCode verifizierungsCode){
     }
 }
